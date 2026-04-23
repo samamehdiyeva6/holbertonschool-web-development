0c5a2e579c34cfcc51c74381a5d6fdadf5a907ad
Your Name <you@example.com>
2026-04-23 18:52:00 +0400

    gitpusher

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized():
+    """Endpoint that triggers 401 error"""
+    abort(401)
