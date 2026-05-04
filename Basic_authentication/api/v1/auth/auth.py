78b90ddd5966ed0b3027977c892dfd12ab5a2c67
Your Name <you@example.com>
2026-05-04 23:35:23 +0400

    gitpusher

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,17 @@
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """Auth class to manage the API authentication."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method that returns True if the path is not in the list of excluded paths."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Method that returns the value of the Authorization header."""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Method that returns the current user."""
+        return None
