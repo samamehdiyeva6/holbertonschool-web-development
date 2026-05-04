9026758ba7d7cab875f61a2bc91dfe00c2864ad8
Your Name <you@example.com>
2026-05-04 18:05:21 +0400

    gitpusher

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized():
       - unauthorized error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """GET /api/v1/forbidden
+    Return:
+      - forbidden error
+    """
+    abort(403)
