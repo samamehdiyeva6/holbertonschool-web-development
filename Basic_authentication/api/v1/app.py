64d5b4ac36dff29afd6fd207466ce2a0e6878401
Your Name <you@example.com>
2026-05-04 16:20:19 +0400

    gitpusher

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,12 +14,6 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
 @app.errorhandler(401)
 def handle_401(error) -> str:
     """
