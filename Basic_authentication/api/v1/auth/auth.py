ec1a153d1e375288486f4ffeea9047a1d3b29a0d
Your Name <you@example.com>
2026-05-06 15:47:45 +0400

    gitpusher

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -17,9 +17,15 @@ class Auth:
             excluded_paths == []
             ):
             return True
-        if path[-1] != '/':
-            return True
-        return False
+        # ensure path ends with /
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded in excluded_paths:
+            if not excluded.endswith('/'):
+                excluded += '/'
+            if path == excluded:
+                return False
 
     def authorization_header(self, request=None) -> str:
         """Method that returns the value of the Authorization header."""
