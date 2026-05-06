b7a307fb68a2096f0286bf46c49d91d5b953c231
Your Name <you@example.com>
2026-05-06 18:11:07 +0400

    gitpusher

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -6,7 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from auth import Auth
+from api.v1.auth.auth import Auth
 import os
 
 
