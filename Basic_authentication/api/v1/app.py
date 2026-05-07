96666af29e70bdaf065cd6e402b605ca1abbe0ff
Your Name <you@example.com>
2026-05-07 14:57:06 +0400

    gitpusher

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -38,8 +38,6 @@ auth = None
 if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-
-
 @app.before_request
 def before_request():
     """Before request handler
