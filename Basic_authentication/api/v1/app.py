2a4feb52c20725ddaaa752fc044e436efc5c1ec2
Your Name <you@example.com>
2026-05-07 14:59:10 +0400

    gitpusher

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = os.getenv('AUTH_TYPE')
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -30,22 +38,19 @@ def unauthorized(error) -> str:
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler
+    """Forbidden error
     """
     return jsonify({"error": "Forbidden"}), 403
 
-auth = None
-if getenv("AUTH_TYPE") == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+
 @app.before_request
-def before_request():
-    """Before request handler
+def before_request_func():
+    """Before_request function
     """
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
+    if not auth.require_auth(request.path, paths):
         return
     if auth.authorization_header(request) is None:
         abort(401)
@@ -55,5 +60,5 @@ def before_request():
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5001")
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
